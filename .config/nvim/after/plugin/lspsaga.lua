ee08a7a9c95cf3ddb39cf07ccc61dbb3d3f3beed
Alan a2n Bouteiller <a2n.dev@pm.me>
2023-01-13 14:55:09 +0100

    migrate lspsaga to new config

--- .config/nvim/after/plugin/lspsaga.lua
+++ .config/nvim/after/plugin/lspsaga.lua
@@ -1,8 +1,7 @@
-
 local status, saga = pcall(require, 'lspsaga')
 if (not status) then return end
 
-saga.init_lsp_saga {
+saga.setup {
   server_filetype_map = {
     typescript = 'typescript'
   }
@@ -16,3 +15,4 @@ vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+
